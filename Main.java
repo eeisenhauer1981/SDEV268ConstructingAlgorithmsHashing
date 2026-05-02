d1374c983e91c453640d116b0a8da4db3b8f2b38
eeisenhauer1981 <eeisenhauer@ivytech.edu>
2026-05-02 11:24:41 -0400

    adding files to repository

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Employee testEmployee = new Employee(1, "Mary", "Tester", true, LocalDate.of(1999, 01, 01));
+        
+
+    }
+}
---
+++ SecurityUtil.java
@@ -0,0 +1,34 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SecurityUtil {
+
+    public static String hashMD5(String input) {
+        try {
+            System.out.println("Input Password: " + input);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : messageDigest) {
+                String hex = Integer.toHexString(0xff & b);
+
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+
+                hexString.append(hex);
+            }
+
+            System.out.println("Hashed Password: " + hexString.toString());
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 algorithm not available", e);
+        }
+
+    }
+    
+}
